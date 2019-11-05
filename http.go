0c104dc26ca9c047c8bed288b1acee66bf0bc119
Pablo Jaime Villarruel <pablolibo@gmail.com>
2019-11-05 16:35:43 -0300

    added code

--- http.go
+++ http.go
@@ -8,24 +8,27 @@ import (
 )
 
 func main() {
+    version := "1.1"
     port := os.Getenv("PORT")
     if port == "" {
-        port = "8080"
+        port = "8000"
     }
 
+    fmt.Fprintf(os.Stdout, "start :%s\n", version)
+
     fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
+
     hostname, _ := os.Hostname()
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
  	fmt.Fprintf(w, "I'm %s\n", hostname)
     })
-    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(os.Stdout, "Version 1")
- 	fmt.Fprintf(w, "version 1")
+
+    http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(os.Stdout, "%s\n", version)
+ 	fmt.Fprintf(w, "%s\n", version)
     })
-
-
 
     log.Fatal(http.ListenAndServe(":" + port, nil))
 }
-
